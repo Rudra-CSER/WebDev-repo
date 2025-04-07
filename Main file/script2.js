fa62d6f637ea7a625c5296019ce18033fb456153
Rudra-CSER <97387423+Rudra-CSER@users.noreply.github.com>
2025-04-07 19:39:02 +0000

    this  & arrow final version

--- Main file/script2.js
+++ Main file/script2.js
@@ -1,1 +1,13 @@
 // this opertor 
+ // this creates current context 
+  //it will be used to accees the elemets inside this scope
+
+const user = {
+    username : "Rudra",
+    price: 999 ,
+
+    welcomeMassage : function(){
+        console.log(`${this.username} , wlcome to our website`);
+        
+    }
+}
