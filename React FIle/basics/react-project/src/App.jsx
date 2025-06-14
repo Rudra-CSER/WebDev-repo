0420134ae2889c031ca71f0b7054cfe1a6322fee
Rudra-CSER <97387423+Rudra-CSER@users.noreply.github.com>
2025-06-14 20:00:29 +0000

    Streak maintained

--- React FIle/basics/react-project/src/App.jsx
+++ React FIle/basics/react-project/src/App.jsx
@@ -3,6 +3,14 @@ import './App.css'
 function App() {
   return (
    <h1>Hello js !!</h1>
+         
+  )
+}
+
+function Descript() {
+  return (
+   <h3>this is my Frist React Webpage</h3>   
+   
   )
 }
 
--- React FIle/basics/react-project/src/main.jsx
+++ React FIle/basics/react-project/src/main.jsx
@@ -2,8 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
 createRoot(document.getElementById('root')).render(
-  
+  <>
     <App />
- 
+    <p>This is my react page</p>
+ </>
 )
